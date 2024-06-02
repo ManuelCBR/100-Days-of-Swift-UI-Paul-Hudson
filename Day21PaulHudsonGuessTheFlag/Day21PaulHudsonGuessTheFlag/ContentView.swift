413ec96feab91a29b8fbec51339315d4ae25f8a2
ManuelCBR <manuelvs1987@gmail.com>
2024-06-03 01:16:30 +0200

    Add day 21 of Paul Hudson 100 days of SwiftUI (Project 2, part two)

---
+++ Day21PaulHudsonGuessTheFlag/Day21PaulHudsonGuessTheFlag/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Day21PaulHudsonGuessTheFlag
+//
+//  Created by Manuel Bermudo on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State var counterScore = 0
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack{
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                VStack(spacing: 15){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3){number in
+                        Button{
+                            flagTapped(number)
+                        }label: {
+                            Image(countries[number])
+                                .clipShape(.buttonBorder)
+                                .shadow(radius: 10)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(counterScore)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        }message: {
+            Text("Your score is \(counterScore)")
+        }
+    }
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+            counterScore += 1
+            
+        }else{
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day21PaulHudsonGuessTheFlag/Day21PaulHudsonGuessTheFlag/Day21PaulHudsonGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day21PaulHudsonGuessTheFlagApp.swift
+//  Day21PaulHudsonGuessTheFlag
+//
+//  Created by Manuel Bermudo on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Day21PaulHudsonGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
