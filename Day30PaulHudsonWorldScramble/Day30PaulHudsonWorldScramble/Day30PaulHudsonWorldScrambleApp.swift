094c286810b4ab315414bac19d9acfc7a3decf35
ManuelCBR <manuelvs1987@gmail.com>
2024-06-09 14:23:09 +0200

    Add day 30 of Paul Hudson 100 days of SwiftUI (Project 5, part two)

---
+++ Day30PaulHudsonWorldScramble/Day30PaulHudsonWorldScramble/Day30PaulHudsonWorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day30PaulHudsonWorldScrambleApp.swift
+//  Day30PaulHudsonWorldScramble
+//
+//  Created by Manuel Bermudo on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Day30PaulHudsonWorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
