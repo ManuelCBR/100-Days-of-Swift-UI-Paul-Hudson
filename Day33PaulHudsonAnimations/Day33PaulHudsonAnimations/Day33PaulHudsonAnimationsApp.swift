ca35335f1b187d781522c70acd57f416953fd0ac
ManuelCBR <manuelvs1987@gmail.com>
2024-06-10 16:55:03 +0200

    Add Day 33 of Paul Hudson 100 days of SwiftUI (Project 6, part two)

---
+++ Day33PaulHudsonAnimations/Day33PaulHudsonAnimations/Day33PaulHudsonAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day33PaulHudsonAnimationsApp.swift
+//  Day33PaulHudsonAnimations
+//
+//  Created by Manuel Bermudo on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Day33PaulHudsonAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
