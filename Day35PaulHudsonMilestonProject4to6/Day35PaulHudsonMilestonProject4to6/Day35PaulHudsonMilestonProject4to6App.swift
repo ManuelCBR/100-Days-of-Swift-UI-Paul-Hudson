1e13120b7a1e0cd017faf93a8dee5dae7fcc91d3
ManuelCBR <manuelvs1987@gmail.com>
2024-06-17 08:14:50 +0200

    Add Day 35 of Paul Hudson 100 days of SwiftUI (Milestone: Projects 4-6)

---
+++ Day35PaulHudsonMilestonProject4to6/Day35PaulHudsonMilestonProject4to6/Day35PaulHudsonMilestonProject4to6App.swift
@@ -0,0 +1,17 @@
+//
+//  Day35PaulHudsonMilestonProject4to6App.swift
+//  Day35PaulHudsonMilestonProject4to6
+//
+//  Created by Manuel Bermudo on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Day35PaulHudsonMilestonProject4to6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
