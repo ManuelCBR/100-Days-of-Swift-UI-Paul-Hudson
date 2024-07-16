73f0300c33799b7ce25d183e25251b5e1ea6317b
ManuelCBR <manuelvs1987@gmail.com>
2024-07-16 14:27:11 +0200

    Add day 66 of Paul Hudson 100 days of SwiftUI (Project 13, part five)

---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day66PaulHudsonInstafilter/Day66PaulHudsonInstafilter/Day66PaulHudsonInstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day66PaulHudsonInstafilterApp.swift
+//  Day66PaulHudsonInstafilter
+//
+//  Created by Manuel Bermudo on 16/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Day66PaulHudsonInstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
