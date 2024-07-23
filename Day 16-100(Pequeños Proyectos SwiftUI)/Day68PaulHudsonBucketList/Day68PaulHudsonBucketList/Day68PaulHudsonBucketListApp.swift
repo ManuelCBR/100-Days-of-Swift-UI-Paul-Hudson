1ba855e7a628683cc36880ca2414a657140c5ddf
ManuelCBR <manuelvs1987@gmail.com>
2024-07-23 20:24:16 +0100

    Add day 68 of Paul Hudson 100 days of SwiftUI (Project 14, part one)

---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day68PaulHudsonBucketList/Day68PaulHudsonBucketList/Day68PaulHudsonBucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day68PaulHudsonBucketListApp.swift
+//  Day68PaulHudsonBucketList
+//
+//  Created by Manuel Bermudo on 19/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Day68PaulHudsonBucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
