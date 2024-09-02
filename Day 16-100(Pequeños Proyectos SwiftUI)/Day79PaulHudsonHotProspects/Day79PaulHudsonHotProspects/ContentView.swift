e27bbc43a97cd24a013129120ec2621b1fec9b35
ManuelCBR <manuelvs1987@gmail.com>
2024-09-02 16:26:17 +0200

    Add day 79 of Paul Hudson 100 days of SwiftUI (Project 16, part 1)

--- Day 16-100(Pequeños Proyectos SwiftUI)/Day79PaulHudsonHotProspects/Day79PaulHudsonHotProspects/ContentView.swift
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day79PaulHudsonHotProspects/Day79PaulHudsonHotProspects/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
+    @State private var selection = Set<String>()
+    @State private var selectedTab = "One"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users, id: \.self, selection: $selection){user in
+            Text(user)
         }
-        .padding()
+        if selection.isEmpty == false {
+            Text("You selected \(selection.formatted())")
+        }
+        EditButton()
+        TabView(selection: $selectedTab) {
+            Button("Show tab 2"){
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+            
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
     }
 }
 
