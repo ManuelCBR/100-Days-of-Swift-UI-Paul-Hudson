3a2f96e00910da1e5f0f9d8f6bbc8038e52d585e
ManuelCBR <manuelvs1987@gmail.com>
2024-09-02 16:25:50 +0200

    Add day 79 of Paul Hudson 100 days of SwiftUI (Project 16, part 1)

---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day79PaulHudsonHotProspects/Day79PaulHudsonHotProspects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day79PaulHudsonHotProspects
+//
+//  Created by Manuel Bermudo on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day79PaulHudsonHotProspects/Day79PaulHudsonHotProspects/Day79PaulHudsonHotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day79PaulHudsonHotProspectsApp.swift
+//  Day79PaulHudsonHotProspects
+//
+//  Created by Manuel Bermudo on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Day79PaulHudsonHotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
