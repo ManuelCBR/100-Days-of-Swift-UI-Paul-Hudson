0829e8edf3df74d68853a29d3eefb7d022262bf0
ManuelCBR <manuelvs1987@gmail.com>
2024-09-08 12:01:33 +0200

    Add day 88 of Paul Hudson 100 days of SwiftUI (Project 17, part three)

---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day88PaulHudsonFlashzilla/Day88PaulHudsonFlashzilla/Day88PaulHudsonFlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day88PaulHudsonFlashzillaApp.swift
+//  Day88PaulHudsonFlashzilla
+//
+//  Created by Manuel Bermudo on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Day88PaulHudsonFlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
