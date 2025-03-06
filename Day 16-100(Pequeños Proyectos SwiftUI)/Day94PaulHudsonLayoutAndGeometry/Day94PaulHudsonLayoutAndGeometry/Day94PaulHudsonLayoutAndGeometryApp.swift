d7d4e766f77f2c4a5c42f7aa872a1cadb0288a86
ManuelCBR <manuelvs1987@gmail.com>
2025-03-06 11:26:12 +0100

    Prueba Day 94

---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day94PaulHudsonLayoutAndGeometry/Day94PaulHudsonLayoutAndGeometry/Day94PaulHudsonLayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day94PaulHudsonLayoutAndGeometryApp.swift
+//  Day94PaulHudsonLayoutAndGeometry
+//
+//  Created by Manuel Bermudo on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Day94PaulHudsonLayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
