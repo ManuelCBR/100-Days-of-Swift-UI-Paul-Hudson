32749fa6ae33009e73755692a78147a05f5dfa60
ManuelCBR <manuelvs1987@gmail.com>
2025-03-09 12:55:39 +0100

    Add day 97 of Paul Hudson 100 days of SwiftUI (Project 19, part 2)

---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day97PaulHudsonSnowSeeker/Day97PaulHudsonSnowSeeker/Day97PaulHudsonSnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day97PaulHudsonSnowSeekerApp.swift
+//  Day97PaulHudsonSnowSeeker
+//
+//  Created by Manuel Bermudo on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Day97PaulHudsonSnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 16-100(Pequeños Proyectos SwiftUI)/Day97PaulHudsonSnowSeeker/Day97PaulHudsonSnowSeeker/SkyDetailsView.swift
@@ -0,0 +1,38 @@
+//
+//  SkyDetailsView.swift
+//  Day97PaulHudsonSnowSeeker
+//
+//  Created by Manuel Bermudo on 9/3/25.
+//
+
+import SwiftUI
+
+struct SkyDetailsView: View {
+    
+    let resort: Resort
+    
+    var body: some View {
+        Group{
+            VStack{
+                Text("Elevation")
+                    .font(.caption.bold())
+                
+                Text("\(resort.elevation) m")
+                    .font(.title3)
+            }
+            
+            VStack{
+                Text("Snow")
+                    .font(.caption.bold())
+                
+                Text("\(resort.snowDepth) cm")
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    SkyDetailsView(resort: .example)
+}
